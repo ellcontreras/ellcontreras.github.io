17fba0b9f93c04a140fa9f34ad56981618d67863
Luis Contreras Guerra <luisguerra495@gmail.com>
2018-03-06 23:39:16 -0600

    Borrado de versión anterior,integración de chat

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
-'use strict'
+// 'use strict'
 
-var frippedWeb = document.getElementById('frippedWeb');
-var frippedDiv = document.getElementById('frippedDiv');
+// var frippedWeb = document.getElementById('frippedWeb');
+// var frippedDiv = document.getElementById('frippedDiv');
 
-var box = document.getElementById('box');
-var imgPerfil = document.getElementById('imgPerfil');
+// var box = document.getElementById('box');
+// var imgPerfil = document.getElementById('imgPerfil');
 
-box.addEventListener("mouseenter",() => {
-    box.style.display = "none";
-    imgPerfil.style.display = 'inherit';
-});
+// box.addEventListener("mouseenter",() => {
+//     box.style.display = "none";
+//     imgPerfil.style.display = 'inherit';
+// });
 
-box.addEventListener("mouseout", () => {
-    window.setTimeout(() => {
-        box.style.display = "inherit";
-        imgPerfil.style.display = 'none';
-    }, 100);
-});
+// box.addEventListener("mouseout", () => {
+//     window.setTimeout(() => {
+//         box.style.display = "inherit";
+//         imgPerfil.style.display = 'none';
+//     }, 100);
+// });
 
-frippedWeb.onmouseover = () => {
-    frippedDiv.innerHTML = '<h2>Fripped web.</h2><p>Página web de la startup fripped.</p>';
-    document.getElementById('proyectoFripped').className = "proyecto";
-};
-frippedWeb.onmouseout = () => {
-    while(frippedDiv.firstChild){
-        frippedDiv.removeChild(frippedDiv.firstChild);
-    }
+// frippedWeb.onmouseover = () => {
+//     frippedDiv.innerHTML = '<h2>Fripped web.</h2><p>Página web de la startup fripped.</p>';
+//     document.getElementById('proyectoFripped').className = "proyecto";
+// };
+// frippedWeb.onmouseout = () => {
+//     while(frippedDiv.firstChild){
+//         frippedDiv.removeChild(frippedDiv.firstChild);
+//     }
 
-    document.getElementById('proyectoFripped').classList.remove("proyecto");
-};
+//     document.getElementById('proyectoFripped').classList.remove("proyecto");
+// };
