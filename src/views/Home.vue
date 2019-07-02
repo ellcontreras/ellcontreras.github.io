230c5d4239540c253d248ebe2ca3e3446d4ea74c
Luis Contreras Guerra <luisguerra495@gmail.com>
2019-07-02 18:53:49 -0500

    Imported image and update home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,13 @@
 <template lang="pug">
-  .bg-blue-100.text-white
-    h1 Hola
+  div
+    .bg-gray-900.h-screen
+      p.text-center.text-red-600.font-black.font-mono.pt-3 PROGRAMMER & MUSICIAN
+
+      img(src="@/assets/developer.svg" class="w-2/5").mx-auto.mt-32
+      p.text-white.text-center.text-2xl Luis Contreras Guerra
+
+    .h-full.bg-red-700
+      p acsdcs
 </template>
 
 <script>
